2ac088d51600e253318d263a28e2849fa01055ef
khadetou <khadetou96@gmail.com>
2021-12-18 10:17:38 +0000

    product controller & productRespository added in product module

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,7 +1,14 @@
 import { ProductsService } from './products.service';
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { Product } from './product.entity';
 
 @Controller('products')
 export class ProductsController {
   constructor(private productsService: ProductsService) {}
+
+  @Post()
+  createProduct(@Body() createProductDto: CreateProductDto): Promise<Product> {
+    return this.productsService.createProduct(createProductDto);
+  }
 }
