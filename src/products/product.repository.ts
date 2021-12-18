2a5d42f8c8f5a0cf5e33f627201bc09c5f5efe7d
khadetou <khadetou96@gmail.com>
2021-12-18 10:01:00 +0000

    product repository create

--- src/products/product.repository.ts
+++ src/products/product.repository.ts
@@ -1,5 +1,39 @@
+import { CreateProductDto } from './dto/create-product.dto';
 import { EntityRepository, Repository } from 'typeorm';
 import { Product } from './product.entity';
 
 @EntityRepository(Product)
-export class ProductRepository extends Repository<Product> {}
+export class ProductRepository extends Repository<Product> {
+  //...
+
+  async createProduct(createProductDto: CreateProductDto): Promise<Product> {
+    const {
+      name,
+      image,
+      brand,
+      category,
+      store,
+      location,
+      description,
+      price,
+      rating,
+      reviews,
+      stock,
+    } = createProductDto;
+    const product = this.create({
+      name,
+      image,
+      brand,
+      category,
+      store,
+      location,
+      description,
+      price,
+      rating,
+      reviews,
+      stock,
+    });
+    await this.save(product);
+    return product;
+  }
+}
