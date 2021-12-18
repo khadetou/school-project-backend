e6278799163c15f264c13effc1c21b9c63783a9e
khadetou <khadetou96@gmail.com>
2021-12-18 11:56:25 +0000

    update product

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -28,6 +28,42 @@ export class ProductsService {
     return this.productRepository.createProduct(createProductDto);
   }
 
+  async updateProduct(
+    id: string,
+    createProductDto: CreateProductDto,
+  ): Promise<Product> {
+    const {
+      name,
+      image,
+      brand,
+      category,
+      store,
+      location,
+      description,
+      price,
+      rating,
+      reviews,
+      stock,
+      status,
+    } = createProductDto;
+    const product = await this.getProductById(id);
+
+    product.name = name || product.name;
+    product.image = image || product.image;
+    product.brand = brand || product.brand;
+    product.category = category || product.category;
+    product.store = store || product.store;
+    product.location = location || product.location;
+    product.description = description || product.description;
+    product.price = price || product.price;
+    product.rating = rating || product.rating;
+    product.reviews = reviews || product.reviews;
+    product.stock = stock || product.stock;
+    product.status = status || product.status;
+
+    return this.productRepository.save(product);
+  }
+
   async deleteProduct(id: string): Promise<void> {
     const result = await this.productRepository.delete(id);
 
