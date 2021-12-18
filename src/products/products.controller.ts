bac4db2041cb102dd3c8b73b07abcf328496e1f3
khadetou <khadetou96@gmail.com>
2021-12-18 11:38:03 +0000

    get by id controller

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,6 +1,6 @@
 import { GetProductFilterDto } from './dto/get-product-filter.dto';
 import { ProductsService } from './products.service';
-import { Body, Controller, Get, Post, Query } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { Product } from './product.entity';
 
@@ -11,6 +11,11 @@ export class ProductsController {
   @Get()
   getProducts(@Query() filterDto: GetProductFilterDto): Promise<Product[]> {
     return this.productsService.getProducts(filterDto);
+  }
+
+  @Get('/:id')
+  getProductById(@Param('id') id: string): Promise<Product> {
+    return this.productsService.getProductById(id);
   }
 
   @Post()
